bc62757d07815afb6d5d6c79f86c6b96c17cb896
Forrest <rcg4u@users.noreply.github.com>
2024-11-26 21:00:22 -0600

    Update tiktokbot.py
    improve readability of code

--- src/core/tiktokbot.py
+++ src/core/tiktokbot.py
@@ -3,6 +3,7 @@ import os
 import re
 import sys
 import time
+from typing import Optional
 
 import ffmpeg
 
@@ -13,201 +14,148 @@ from http_utils.http_client import HttpClient
 
 
 class TikTok:
-
-    def __init__(self, httpclient, output, mode, logger, cookies, url=None,
-                 user=None, room_id=None, use_ffmpeg=None, duration=None,
-                 convert=False):
-
-        # TikTok
+    def __init__(self, httpclient: HttpClient, output: str, mode: Mode, logger, cookies: dict,
+                 url: Optional[str] = None, user: Optional[str] = None, room_id: Optional[str] = None,
+                 use_ffmpeg: Optional[bool] = None, duration: Optional[int] = None, convert: bool = False):
         self.url = url
         self.user = user
         self.room_id = room_id
 
-        # Tool Settings
         self.httpclient = httpclient.req
         self.mode = mode
         self.cookies = cookies
 
-        # Recording Settings
         self.use_ffmpeg = use_ffmpeg
         self.duration = duration
         self.convert = convert
 
-        # Output & Results
         self.output = output
         self.logger = logger
 
-        # Check if the user's country is blacklisted
-        is_blacklisted = self.is_country_blacklisted()
-        if is_blacklisted:
+        if self.is_country_blacklisted():
             if room_id is None:
                 raise CountryBlacklisted(Error.BLACKLIST_ERROR)
             if mode == Mode.AUTOMATIC:
                 raise ValueError(Error.AUTOMATIC_MODE_ERROR)
 
-        # Get live information based on the provided user data
-        if self.url is not None:
+        if self.url:
             self.get_room_and_user_from_url()
-                     
-        if self.user is None:
+        if not self.user:
             self.user = self.get_user_from_room_id()
-
-        if self.room_id is None:
+        if not self.room_id:
             self.room_id = self.get_room_id_from_user()
 
         self.logger.info(f"USERNAME: {self.user}")
-        if self.room_id == "":
+        if not self.room_id:
             self.logger.info(f"ROOM_ID: {Error.USER_NEVER_BEEN_LIVE}")
         else:
             self.logger.info(f"ROOM_ID:  {self.room_id}")
 
-        # Create a new httpclient without proxy
-        self.httpclient = HttpClient(
-            self.logger, cookies=self.cookies, proxy=None
-        ).req
+        self.httpclient = HttpClient(self.logger, cookies=self.cookies, proxy=None).req
 
     def run(self):
-        """
-        runs the program in the selected mode. 
-        
-        If the mode is MANUAL, it checks if the user is currently live and if so, starts recording. 
-        
-        If the mode is AUTOMATIC, it continuously checks if the user is live and if not, waits for the specified timeout before rechecking.
-        If the user is live, it starts recording.
-        """
         if self.mode == Mode.MANUAL:
-
-            if self.room_id == "":
+            if not self.room_id:
                 raise UserNotLiveException(Error.USER_NEVER_BEEN_LIVE)
-
             if not self.is_user_in_live():
                 raise UserNotLiveException(Error.USER_NOT_CURRENTLY_LIVE)
-
             self.start_recording()
+        elif self.mode == Mode.AUTOMATIC:
+            self.run_automatic_mode()
 
-        if self.mode == Mode.AUTOMATIC:
-            while True:
+    def run_automatic_mode(self):
+        while True:
+            try:
+                self.room_id = self.get_room_id_from_user()
+                if not self.room_id:
+                    raise UserNotLiveException(Error.USER_NEVER_BEEN_LIVE)
+                if not self.is_user_in_live():
+                    raise UserNotLiveException(Error.USER_NOT_CURRENTLY_LIVE)
+                self.start_recording()
+            except UserNotLiveException as ex:
+                self.logger.info(ex)
+                self.logger.info(f"Waiting {TimeOut.AUTOMATIC_MODE} minutes before recheck\n")
+                time.sleep(TimeOut.AUTOMATIC_MODE * TimeOut.ONE_MINUTE)
+            except ConnectionAbortedError:
+                self.logger.error(Error.CONNECTION_CLOSED_AUTOMATIC)
+                time.sleep(TimeOut.CONNECTION_CLOSED * TimeOut.ONE_MINUTE)
+            except Exception as ex:
+                self.logger.error(ex)
 
-                try:
-
-                    self.room_id = self.get_room_id_from_user()
-                    if self.room_id == "":
-                        raise UserNotLiveException(Error.USER_NEVER_BEEN_LIVE)
-
-                    if not self.is_user_in_live():
-                        raise UserNotLiveException(Error.USER_NOT_CURRENTLY_LIVE)
-
-                    self.start_recording()
-
-                except UserNotLiveException as ex:
-                    self.logger.info(ex)
-                    self.logger.info(f"waiting {TimeOut.AUTOMATIC_MODE} minutes before recheck\n")
-                    time.sleep(TimeOut.AUTOMATIC_MODE * TimeOut.ONE_MINUTE)
-
-                except ConnectionAbortedError:
-                    self.logger.error(Error.CONNECTION_CLOSED_AUTOMATIC)
-                    time.sleep(TimeOut.CONNECTION_CLOSED * TimeOut.ONE_MINUTE)
-
-                except Exception as ex:
-                    self.logger.error(ex)
-
-    def convertion_mp4(self, file):
-        """
-        Convert the video from flv format to mp4 format
-        """
+    def convertion_mp4(self, file: str):
         try:
-            self.logger.info("Converting {} to MP4 format...".format(file))
+            self.logger.info(f"Converting {file} to MP4 format...")
             ffmpeg.input(file).output(file.replace('_flv.mp4', '.mp4'), y='-y').run(quiet=True)
             os.remove(file)
-            self.logger.info("Finished converting {}".format(file))
+            self.logger.info(f"Finished converting {file}")
         except FileNotFoundError:
             self.logger.error("FFmpeg is not installed. -> pip install ffmpeg-python")
 
     def start_recording(self):
-        """
-        Start recording live
-        """
         live_url = self.get_live_url()
         if not live_url:
             raise LiveNotFound(Error.URL_NOT_FOUND)
 
         current_date = time.strftime("%Y.%m.%d_%H-%M-%S", time.localtime())
+        output_path = self.prepare_output_path(current_date)
 
-        # TO CHANGE
-        if (self.output != "" and isinstance(self.output, str) and not
-        (self.output.endswith('/') or self.output.endswith('\\'))):
-            if os.name == 'nt':
-                self.output = self.output + "\\"
-            else:
-                self.output = self.output + "/"
-
-        output = f"{self.output if self.output else ''}TK_{self.user}_{current_date}_flv.mp4"
-
-        print("")
-        if self.duration:
-            self.logger.info(f"START RECORDING FOR {self.duration} SECONDS ")
-        else:
-            self.logger.info("STARTED RECORDING...")
+        self.logger.info(f"Start recording for {self.duration} seconds" if self.duration else "Started recording...")
 
         try:
             if self.use_ffmpeg:
-                self.logger.info("[PRESS 'q' TO STOP RECORDING]")
-                stream = ffmpeg.input(live_url)
-
-                if self.duration is not None:
-                    stream = ffmpeg.output(stream, output.replace("_flv.mp4", ".mp4"), c='copy', t=self.duration)
-                else:
-                    stream = ffmpeg.output(stream, output.replace("_flv.mp4", ".mp4"), c='copy')
-
-                ffmpeg.run(stream, quiet=True)
+                self.record_with_ffmpeg(live_url, output_path)
             else:
-                self.logger.info("[PRESS ONLY ONCE CTRL + C TO STOP]")
-                response = self.httpclient.get(live_url, stream=True)
-                with open(output, "wb") as out_file:
-                    start_time = time.time()
-                    for chunk in response.iter_content(chunk_size=4096):
-                        out_file.write(chunk)
-                        elapsed_time = time.time() - start_time
-                        if self.duration is not None and elapsed_time >= self.duration:
-                            break
-
+                self.record_with_stream(live_url, output_path)
         except ffmpeg.Error as e:
             self.logger.error('FFmpeg Error:')
             self.logger.error(e.stderr.decode('utf-8'))
-
         except FileNotFoundError:
             self.logger.error("FFmpeg is not installed -> pip install ffmpeg-python")
             sys.exit(1)
-
         except KeyboardInterrupt:
             pass
 
-        self.logger.info(f"FINISH: {output}\n")
+        self.logger.info(f"FINISH: {output_path}\n")
 
-        if self.use_ffmpeg:
-            return
+        if not self.use_ffmpeg and not self.convert:
+            if input("Do you want to convert it to real mp4? [Y/N] -> ").lower() == "y":
+                self.convertion_mp4(output_path)
+        elif self.convert:
+            self.convertion_mp4(output_path)
 
-        if not self.convert:
-            self.logger.info("Do you want to convert it to real mp4? [Requires ffmpeg installed] -> pip install ffmpeg-python")
-            choice = input("Y/N -> ")
-            if choice.lower() == "y":
-                self.convertion_mp4(output)
+    def prepare_output_path(self, current_date: str) -> str:
+        output_dir = self.output
+        if output_dir and not (output_dir.endswith('/') or output_dir.endswith('\\')):
+            output_dir += "\\" if os.name == 'nt' else "/"
+        return f"{output_dir if output_dir else ''}TK_{self.user}_{current_date}_flv.mp4"
+
+    def record_with_ffmpeg(self, live_url: str, output: str):
+        self.logger.info("[PRESS 'q' TO STOP RECORDING]")
+        stream = ffmpeg.input(live_url)
+        if self.duration:
+            stream = ffmpeg.output(stream, output.replace("_flv.mp4", ".mp4"), c='copy', t=self.duration)
         else:
-            self.convertion_mp4(output)
+            stream = ffmpeg.output(stream, output.replace("_flv.mp4", ".mp4"), c='copy')
+        ffmpeg.run(stream, quiet=True)
+
+    def record_with_stream(self, live_url: str, output: str):
+        self.logger.info("[PRESS ONLY ONCE CTRL + C TO STOP]")
+        response = self.httpclient.get(live_url, stream=True)
+        with open(output, "wb") as out_file:
+            start_time = time.time()
+            for chunk in response.iter_content(chunk_size=4096):
+                out_file.write(chunk)
+                if self.duration and (time.time() - start_time) >= self.duration:
+                    break
 
     def get_live_url(self) -> str:
-        """
-        I get the cdn (flv or m3u8) of the streaming
-        """
         url = f"https://webcast.tiktok.com/webcast/room/info/?aid=1988&room_id={self.room_id}"
         data = self.httpclient.get(url).json()
 
         if 'This account is private' in data:
             raise AccountPrivate
 
-        live_url_flv = data.get(
-            'data', {}).get('stream_url', {}).get('rtmp_pull_url', None)
-
+        live_url_flv = data.get('data', {}).get('stream_url', {}).get('rtmp_pull_url', None)
         if live_url_flv is None and data.get('status_code') == 4003110:
             raise LiveRestriction
 
@@ -216,100 +164,60 @@ class TikTok:
         return live_url_flv
 
     def is_user_in_live(self) -> bool:
-        """
-        Checking whether the user is live.
-        """
-        data = self.httpclient.get(
-            "https://webcast.tiktok.com:443/webcast/room/check_alive/"
-            f"?aid=1988&region=CH&room_ids={self.room_id}&user_is_login=true"
-        ).json()
+        url = f"https://webcast.tiktok.com:443/webcast/room/check_alive/?aid=1988&region=CH&room_ids={self.room_id}&user_is_login=true"
+        data = self.httpclient.get(url).json()
 
-        if 'data' not in data or len(data['data']) == 0:
-            return False
-
-        return data['data'][0].get('alive', False)
+        return data.get('data', [{}])[0].get('alive', False)
 
     def get_room_and_user_from_url(self):
-        """
-        Given a url, get user and room_id.
-        """
         response = self.httpclient.get(self.url, allow_redirects=False)
         content = response.text
 
         if response.status_code == StatusCode.REDIRECT:
             raise CountryBlacklisted('Redirect')
 
-        if response.status_code == StatusCode.MOVED:  # MOBILE URL
+        if response.status_code == StatusCode.MOVED:
             matches = re.findall("com/@(.*?)/live", content)
-            if len(matches) < 1:
+            if not matches:
                 raise LiveNotFound(Error.LIVE_NOT_FOUND)
-
             self.user = matches[0]
 
-        # https://www.tiktok.com/@<username>/live
-        match = re.match(
-            r"https?://(?:www\.)?tiktok\.com/@([^/]+)/live",
-            self.url
-        )
+        match = re.match(r"https?://(?:www\.)?tiktok\.com/@([^/]+)/live", self.url)
         if match:
             self.user = match.group(1)
 
         self.room_id = self.get_room_id_from_user()
 
-    def get_room_id_from_user(self):
-        """
-        Given a username, I get the room_id
-        """
-        content = self.httpclient.get(
-            url=f'https://www.tiktok.com/@{self.user}/live'
-        ).text
-
+    def get_room_id_from_user(self) -> str:
+        content = self.httpclient.get(f'https://www.tiktok.com/@{self.user}/live').text
         if 'Please wait...' in content:
             raise IPBlockedByWAF
 
-        pattern = re.compile(
-            r'<script id="SIGI_STATE" type="application/json">(.*?)</script>',
-            re.DOTALL)
-        match = pattern.search(content)
-
+        match = re.search(r'<script id="SIGI_STATE" type="application/json">(.*?)</script>', content, re.DOTALL)
         if not match:
-            raise ValueError("[-] Error extracting roomId")
+            raise ValueError("Error extracting roomId")
 
         data = json.loads(match.group(1))
 
         if 'LiveRoom' not in data and 'CurrentRoom' in data:
             return ""
 
-        room_id = data.get('LiveRoom', {}).get('liveRoomUserInfo', {}).get(
-            'user', {}).get('roomId', None)
-
+        room_id = data.get('LiveRoom', {}).get('liveRoomUserInfo', {}).get('user', {}).get('roomId', None)
         if room_id is None:
             raise ValueError("RoomId not found.")
 
         return room_id
 
     def get_user_from_room_id(self) -> str:
-        """
-        Given a room_id, I get the username
-        """
         url = f"https://www.tiktok.com/api/live/detail/?aid=1988&roomID={self.room_id}"
         data = self.httpclient.get(url).json()
 
-        unique_id = data.get('LiveRoomInfo', {}).get('ownerInfo', {}).get(
-            'uniqueId', None)
-
+        unique_id = data.get('LiveRoomInfo', {}).get('ownerInfo', {}).get('uniqueId', None)
         if not unique_id:
             raise AttributeError(Error.USERNAME_ERROR)
 
         return unique_id
 
     def is_country_blacklisted(self) -> bool:
-        """
-        Checks if the user is in a blacklisted country that requires login
-        """
-        response = self.httpclient.get(
-            f"https://www.tiktok.com/@{self.user}/live",
-            allow_redirects=False
-        )
-
+        response = self.httpclient.get(f"https://www.tiktok.com/@{self.user}/live", allow_redirects=False)
         return response.status_code == StatusCode.REDIRECT
